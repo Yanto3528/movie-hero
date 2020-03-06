9bfe6278aec1d323721da2062a0c972171aa3478
Yanto Lee <yanto3528@gmail.com>
2020-03-06 13:22:27 +0700

    fix url not showing up correctly

--- src/components/layout/AutoComplete.js
+++ src/components/layout/AutoComplete.js
@@ -1,13 +1,14 @@
 import React from "react";
-
 import "./AutoComplete.css";
+
+import convertToUrlString from "../utils/ConvertToUrlString";
 
 const AutoComplete = ({ movies }) => {
   return (
     <div className="auto-complete">
       {movies.map(movie => (
         <div key={movie.id} className="list-movie">
-          <a href={`/movie/${movie.id}/${movie.title}`}>
+          <a href={`/movie/${movie.id}/${convertToUrlString(movie.title)}`}>
             {movie.title} ({movie.release_date.split("-")[0]})
           </a>
         </div>
